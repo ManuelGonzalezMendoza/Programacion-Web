5bef39716a753a6657f64c6affa7be0faa0b6cc6
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-09 23:48:23 -0600

    A

--- Parcial3/Parametros/parametros.js
+++ Parcial3/Parametros/parametros.js
@@ -24,7 +24,7 @@ app.get("/estudiantes/:carrera",(req,res)=>{
     res.send('Servidor contestando a peticion GET con parametros')
     })
 
-const PORT =8082;
+const PORT =3000;
 app.listen(PORT,()=>{
     console.info(`Servidor corriendo en el puerto ${PORT}`);
 });
