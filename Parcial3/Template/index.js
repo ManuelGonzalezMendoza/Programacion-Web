de99f5cf819dfe2946571a509548d0f758cd4f02
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-11 07:34:52 -0600

    Actualizacion

---
+++ Parcial3/Template/index.js
@@ -0,0 +1,41 @@
+const express= require('express');
+const path=require('path')
+const app =express();
+
+
+
+app.use(express.json());
+app.use(express.text());
+
+app.set('view engine','pug');
+app.set('views',path.join(_dirname), 'views');
+
+
+app.get('/',(req,res)=>{res.send('Hola mundo')})
+
+
+app.get("/administrativos",(req,res)=>{
+    console.log(req.query);
+    res.send('Servidor contestando a peticion GET con parametros en el query')
+    res.render(admin)
+    })
+
+app.get("/maestros",(req,res)=>{
+console.log(req.body);
+res.send('Servidor contestando a peticion GET con parametros en el body')
+})
+
+
+app.get("/estudiantes/:carrera",(req,res)=>{
+    console.log(req.params.carrera);
+    console.log(req.params.control);
+    res.send('Servidor contestando a peticion GET con parametros')
+    })
+
+const PORT =3000;
+app.listen(PORT,()=>{
+    console.info(`Servidor corriendo en el puerto ${PORT}`);
+});
+
+
+
