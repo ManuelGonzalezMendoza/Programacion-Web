7aeb9612f93aa89d14d88cc8f8fdec6227bc6813
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-12 07:50:42 -0600

    A

--- Parcial3/Formulario/index.js
+++ Parcial3/Formulario/index.js
@@ -3,13 +3,10 @@ const path=require('path')
 const app =express();
 
 
-
 app.use(express.json());
 app.use(express.text());
 app.use(express.urlencoded({extended:true}))
 
-
-app.get('/',(req,res)=>{res.send('Hola mundo')})
 
 
 app.get("/administrativos",(req,res)=>{
@@ -20,7 +17,7 @@ app.get("/administrativos",(req,res)=>{
 
 app.get("/formulario",(req,res)=>{
 console.log(req.body);
-res.send('Hola  ${PORT}' )
+res.send('Hola  ${req.body.nombre}' )
 })
 
 
