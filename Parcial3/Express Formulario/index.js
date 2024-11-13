1f1456130b6bdac86cfb6607685fdd634905e979
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-13 07:36:12 -0600

    A

---
+++ Parcial3/Express Formulario/index.js
@@ -0,0 +1,23 @@
+const express= require('express');
+const path=require('path')
+const multer= require('multer')
+const app =express();
+
+const folder= path.join(__dirname+'/archivos/')
+const upload = multer({dest:folder})
+
+             
+app.use(express.json());
+app.use(express.text());
+app.use(upload.single('archivo'));  
+
+
+app.post("/formulario",(req,res)=>{
+console.log(req.body);
+res.send( `Hola  ${req.body.nombre}` )
+})
+
+const PORT =3000;
+app.listen(PORT,()=>{
+    console.info(`Servidor corriendo en el puerto ${PORT}`);
+});
