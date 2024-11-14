d11dcd9ed9ad6901b5bbdd6699a741ac8416249e
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-14 07:58:19 -0600

    A

--- Parcial3/FormularioCors/index.js
+++ Parcial3/FormularioCors/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const multer = require("multer");
+var cors = require('cors')
 const app = express();
 
 const folder = path.join(__dirname, 'archivos');
