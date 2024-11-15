6bd46f1137fc376151faddbb01729c127f68238c
Manuel Gonzalez Mendoza <manuelglzmn@gmail.com>
2024-11-15 14:22:29 -0600

    A

--- Parcial3/Formulario copy/index.js
+++ Parcial3/Formulario copy/index.js
@@ -1,20 +1,41 @@
-const express= require('express');
-const path=require('path')
-const app =express();
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const cors = require('cors');
+const jsPDF = require('jspdf');
+const app = express();
 
 
+// Configuración de almacenamiento de multer
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname + '/archivosrec/'));
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now());
+  }
+});
+
+const upload = multer({ storage: storage });
+
+// Middleware
 app.use(express.json());
 app.use(express.text());
-app.use(express.urlencoded({extended:true}))
+app.use(upload.single('archivo'));
+app.use(cors());
 
+// Ruta para manejar el formulario
+app.post('/formulario', (req, res) => {
+  const doc = new jsPDF();
+  doc.text(`Hello ${req.body.nombre}`, 10, 10);
+  
+  let arch = path.join(__dirname + '/archivosgen/a4.pdf');
+  doc.save(arch);
+  
+  res.sendFile(arch);
+});
 
-app.get("/formulario",(req,res)=>{
-console.log(req.body);
-res.send( `Hola  ${req.body.nombre}` )
-})
-
-
-const PORT =3000;
-app.listen(PORT,()=>{
+const PORT = 8081;
+app.listen(PORT, () => {
     console.info(`Servidor corriendo en el puerto ${PORT}`);
 });
